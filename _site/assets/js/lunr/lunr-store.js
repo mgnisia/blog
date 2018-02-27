55c03e57e7de05bfad1233c766bb28e89e66086a
Moritz Gnisia <moritzgnisia@gmail.com>
2018-02-27 08:37:23 +0100

    change

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Erweiterte Funktionen in Python",
+        "excerpt":"Welche Möglichkeiten bestehen Funktionen zu erweitern?Zunächst einmal gilt es zu unterscheiden, welche Parametertypen für eine Funktion grundsätzlich möglich sind. Diese kann man der folgenden Tabelle entnehmen.Theorie Typ Erläuterung Pflichtparameter Diese Parameter müssen bei Aufruf der Funktion vorgegeben werden. Standardparameter Diese Parameter können bei Aufruf der Funktion vorgegeben werden. Falls dies...","categories": [],
+        "tags": ["Python","Funktion"],
+        "url": "http://localhost:4000/Python_Funktionen-Teil2/",
+        "teaser":null},{
+        "title": "Einfache Funktionen in Python",
+        "excerpt":"Wie ist eine Funktion in Python aufgebaut? Worauf sollte man achten?TheorieGrundsätzlich baut sich eine Funktion in Python wie folgt auf:def Funktionsname(Parameter1): \"Beschreibung der Funktion\" variable1 = Parameter1 variable2 = Parameter1 + 10 return variable1, variable2Wesentlichen Eigenschaften sind: Eine Funktion beginnt immer mit dem Keyword def Sie muss einen Funktionsnamen besitzen...","categories": [],
+        "tags": ["Python","Funktion"],
+        "url": "http://localhost:4000/Python_Funktionen/",
+        "teaser":null}]
